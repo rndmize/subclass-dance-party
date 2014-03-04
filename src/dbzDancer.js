45cc285dc75a68eaa9d07b9e67d29ffb10d9cca0
Hack Reactor Students <rndmize@gmail.com>
2014-03-03 20:45:16 -0800

    Bird is sort of flying

--- src/dbzDancer.js
+++ src/dbzDancer.js
@@ -1,7 +1,18 @@
-var count = 0;
+//var count = 0;
 var DBZDancer = function(top, left, timeBetweenSteps){
   Dancer.call(this, top, left, timeBetweenSteps);
   this.$node = $('<span class="fightingDancer"></span>');
+
+  //set timeout
+  $(".fightingDancer").on('mouseover', function(event) {
+    console.log('inside mouseover');
+    //console.log("its working?");
+    $(this).css('top', function(index, value) {
+      console.log(value);
+      var newValue = parseInt(value);
+      return (newValue - 4) + 'px';
+    });
+  });
 
 };
 
@@ -11,17 +22,13 @@ DBZDancer.prototype.constructor = DBZDancer;
 
 DBZDancer.prototype.step = function(){
   Dancer.prototype.step.call(this);
-  count++;
+///count++;
 
-  this.$node.css('background-position-x', count*60);
+//this.$node.css('background-position-x', count*60);
 
-  $(".fightingDancer").mouseover(function(event) {
-    //console.log("its working?");
-    $(this).css('top', function(index, value) {
-      console.log(value);
-      var newValue = parseInt(value);
-      return (newValue - 1) + 'px';
-    });
-  });
+this.$node.css('background-position-x', function(index, value){
+  return (value * 60) + 'px';
+});
+
 
 };
